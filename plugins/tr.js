baf034d5af04d6485be406d5dd4ed8a16c81a05d
Amanda189ty <amandaqween18@gmail.com>
2024-06-09 14:19:10 +0100

    Update tr.js

--- plugins/tr.js
+++ plugins/tr.js
@@ -14,7 +14,7 @@ text = args.join(' ')}
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 try {      
 let result = await translate(`${text}`, { to: lang, autoCorrect: true })
-await m.reply('❀ꪑꪖ𝘳ᧁꪖ𝘳𝓲𝓽ꪖ✾  \n *تمت الترجمة بنجاح☑️*\n\n\n\n' + result.text)
+await m.reply(result.text)
 } catch {
 try {    
 let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
