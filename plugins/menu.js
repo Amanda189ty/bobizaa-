d9f2d04fbee2fe444b34d361a2e0a7562e5d850c
Amanda189ty <amandaqween18@gmail.com>
2024-06-10 09:00:34 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -146,7 +146,6 @@ renderLargerThumbnail: true
 
 conn.sendMessage(m.chat, {audio: { url: 'https://a.uguu.se/bHKLYPKH.mp3' }, mimetype:'audio/mpeg', ptt:true }, {quoted:m})
     
-conn.sendMessage(m.chat, {audio: { url: 'https://a.uguu.se/bHKLYPKH.mp3' }, mimetype:'audio/mpeg', ptt:true }, {quoted:m})
     
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
