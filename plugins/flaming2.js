72e89f8b1b2aa0cd9a36499cb485eed1cb5c8e34
Amanda189ty <amandaqween18@gmail.com>
2024-06-13 08:03:14 +0100

    Update flaming2.js

--- plugins/flaming2.js
+++ plugins/flaming2.js
@@ -1,13 +1,13 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .flaming2 JITOSSA'
+  if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .logo1 SASUKE'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'gura.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'gura.jpg', `> _SASUKE TECH LOGO_`, m, false)
 }
-handler.help = ['flaming2'].map(v => v + ' <text>')
+handler.help = ['logo1'].map(v => v + ' <text>')
 handler.tags = ['maker']
-handler.command = /^(flaming2)$/i
+handler.command = /^(logo1)$/i
 
 export default handler
